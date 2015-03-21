9f2bbff52e7b16de1ce677f7d3e7dc75f8154b97
Kyle Swanson <kylophone@gmail.com>
2015-03-20 19:18:10 -0500

    inital commit

---
+++ ti83font.h
@@ -0,0 +1,6 @@
+/** \brief fills an array with the memory needed to display an ASCII character. Uses TI-83 font.
+* @param this_letter character to be converted.
+* @param mem_array display memory. Allocate 5 bytes.
+* http://tibasicdev.wikidot.com/83lgfont
+*/
+int ti83font(char this_letter, char *mem_array);
