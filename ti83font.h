3f4bc54b983bbb2adb8193eee9296bad3f381ba2
Kyle Swanson <kylophone@gmail.com>
2015-03-20 19:21:10 -0500

    Update ti83font.h

--- ti83font.h
+++ ti83font.h
@@ -1,4 +1,4 @@
-/** \brief fills an array with the memory needed to display an ASCII character. Uses TI-83 font.
+/** \brief Fill a 5 byte array with the memory needed to display low resolution (7x5) ASCII characters. Uses the TI-83 font.
 * @param this_letter character to be converted.
 * @param mem_array display memory. Allocate 5 bytes.
 * http://tibasicdev.wikidot.com/83lgfont
