5c358fb6744bd14d6912450de909c2a435da32e3
Kyle Swanson <kylophone@gmail.com>
2015-03-23 13:08:01 -0500

    added a-z

--- ti83font.c
+++ ti83font.c
@@ -1,5 +1,13 @@
 int ti83font(char this_ascii, char *mem_array) {
 	switch (this_ascii) {
+		case 32: //Space
+			mem_array[0] = 0b00000000;
+			mem_array[1] = 0b00000000;
+			mem_array[2] = 0b00000000;
+			mem_array[3] = 0b00000000;
+			mem_array[4] = 0b00000000;
+			return 0;
+		
 		case 65: //A
 			mem_array[0] = 0b00111111;
 			mem_array[1] = 0b01001000;
@@ -207,13 +215,213 @@ int ti83font(char this_ascii, char *mem_array) {
 			mem_array[3] = 0b01010001;
 			mem_array[4] = 0b01100001;
 			return 0;
+			
+		case 97: //a
+			mem_array[0] = 0b00000010;
+			mem_array[1] = 0b00010101;
+			mem_array[2] = 0b00010101;
+			mem_array[3] = 0b00010101;
+			mem_array[4] = 0b00001111;
+			return 0;
+			
+		case 98: //b
+			mem_array[0] = 0b01111111;
+			mem_array[1] = 0b00001001;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b00010001;
+			mem_array[4] = 0b00001110;
+			return 0;
+			
+		case 99: //c
+			mem_array[0] = 0b00001110;
+			mem_array[1] = 0b00010001;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b00010001;
+			mem_array[4] = 0b00000010;
+			return 0;
 
-		case 32: //Space
+		case 100: //d
+			mem_array[0] = 0b00001110;
+			mem_array[1] = 0b00010001;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b00001001;
+			mem_array[4] = 0b01111111;
+			return 0;
+
+		case 101: //e
+			mem_array[0] = 0b00001110;
+			mem_array[1] = 0b00010101;
+			mem_array[2] = 0b00010101;
+			mem_array[3] = 0b00010101;
+			mem_array[4] = 0b00001100;
+			return 0;
+
+		case 102: //f
+			mem_array[0] = 0b00001000;
+			mem_array[1] = 0b00111111;
+			mem_array[2] = 0b01001000;
+			mem_array[3] = 0b01000000;
+			mem_array[4] = 0b00100000;
+			return 0;
+
+		case 103: //g
+			mem_array[0] = 0b00011000;
+			mem_array[1] = 0b00100101;
+			mem_array[2] = 0b00100101;
+			mem_array[3] = 0b00100101;
+			mem_array[4] = 0b00111110;
+			return 0;
+
+		case 104: //h
+			mem_array[0] = 0b01111111;
+			mem_array[1] = 0b00001000;
+			mem_array[2] = 0b00010000;
+			mem_array[3] = 0b00010000;
+			mem_array[4] = 0b00001111;
+			return 0;
+
+		case 105: //i
 			mem_array[0] = 0b00000000;
-			mem_array[1] = 0b00000000;
-			mem_array[2] = 0b00000000;
-			mem_array[3] = 0b00000000;
+			mem_array[1] = 0b00010001;
+			mem_array[2] = 0b01011111;
+			mem_array[3] = 0b00000001;
 			mem_array[4] = 0b00000000;
+			return 0;
+
+		case 106: //j
+			mem_array[0] = 0b00000010;
+			mem_array[1] = 0b00000001;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b01011110;
+			mem_array[4] = 0b00000000;
+			return 0;
+
+		case 107: //k
+			mem_array[0] = 0b00000000;
+			mem_array[1] = 0b01111111;
+			mem_array[2] = 0b00000100;
+			mem_array[3] = 0b00001010;
+			mem_array[4] = 0b00010001;
+			return 0;
+
+		case 108: //l
+			mem_array[0] = 0b00000000;
+			mem_array[1] = 0b01000001;
+			mem_array[2] = 0b01111111;
+			mem_array[3] = 0b00000001;
+			mem_array[4] = 0b00000000;
+			return 0;
+
+		case 109: //m
+			mem_array[0] = 0b00011111;
+			mem_array[1] = 0b00010000;
+			mem_array[2] = 0b00001100;
+			mem_array[3] = 0b00010000;
+			mem_array[4] = 0b00001111;
+			return 0;
+
+		case 110: //n
+			mem_array[0] = 0b00011111;
+			mem_array[1] = 0b00001000;
+			mem_array[2] = 0b00010000;
+			mem_array[3] = 0b00010000;
+			mem_array[4] = 0b00001111;
+			return 0;
+
+		case 111: //o
+			mem_array[0] = 0b00001110;
+			mem_array[1] = 0b00010001;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b00010001;
+			mem_array[4] = 0b00001110;
+			return 0;
+
+		case 112: //p
+			mem_array[0] = 0b00011111;
+			mem_array[1] = 0b00010100;
+			mem_array[2] = 0b00010100;
+			mem_array[3] = 0b00010100;
+			mem_array[4] = 0b00001000;
+			return 0;
+
+		case 113: //q
+			mem_array[0] = 0b00001000;
+			mem_array[1] = 0b00010100;
+			mem_array[2] = 0b00010100;
+			mem_array[3] = 0b00001100;
+			mem_array[4] = 0b00011111;
+			return 0;
+
+		case 114: //r
+			mem_array[0] = 0b00011111;
+			mem_array[1] = 0b00001000;
+			mem_array[2] = 0b00010000;
+			mem_array[3] = 0b00010000;
+			mem_array[4] = 0b00001000;
+			return 0;
+
+		case 115: //s
+			mem_array[0] = 0b00001001;
+			mem_array[1] = 0b00010101;
+			mem_array[2] = 0b00010101;
+			mem_array[3] = 0b00010101;
+			mem_array[4] = 0b00000010;
+			return 0;
+
+		case 116: //t
+			mem_array[0] = 0b00010000;
+			mem_array[1] = 0b01111110;
+			mem_array[2] = 0b00010001;
+			mem_array[3] = 0b00000001;
+			mem_array[4] = 0b00000010;
+			return 0;
+
+		case 117: //u
+			mem_array[0] = 0b00011110;
+			mem_array[1] = 0b00000001;
+			mem_array[2] = 0b00000001;
+			mem_array[3] = 0b00000010;
+			mem_array[4] = 0b00011111;
+			return 0;
+
+		case 118: //v
+			mem_array[0] = 0b00011100;
+			mem_array[1] = 0b00000010;
+			mem_array[2] = 0b00000001;
+			mem_array[3] = 0b00000010;
+			mem_array[4] = 0b00011100;
+			return 0;
+
+		case 119: //w
+			mem_array[0] = 0b00011110;
+			mem_array[1] = 0b00000001;
+			mem_array[2] = 0b00000110;
+			mem_array[3] = 0b00000001;
+			mem_array[4] = 0b00011110;
+			return 0;
+
+		case 120: //x
+			mem_array[0] = 0b00010001;
+			mem_array[1] = 0b00001010;
+			mem_array[2] = 0b00000100;
+			mem_array[3] = 0b00001010;
+			mem_array[4] = 0b00010001;
+			return 0;
+
+		case 121: //y
+			mem_array[0] = 0b00011000;
+			mem_array[1] = 0b00000101;
+			mem_array[2] = 0b00000101;
+			mem_array[3] = 0b00000101;
+			mem_array[4] = 0b00011110;
+			return 0;
+
+		case 122: //z
+			mem_array[0] = 0b00010001;
+			mem_array[1] = 0b00010011;
+			mem_array[2] = 0b00010101;
+			mem_array[3] = 0b00011001;
+			mem_array[4] = 0b00010001;
 			return 0;
 
 		default: //ASCII character not supported.
